433ed2b77d7ca42ae6e8e07fcfbe18318470a265
André Luiz Da Costa Ravagnani <andreskiter@gmail.com>
2013-03-15 16:08:51 -0300

    Terminado o trabalho do Eduardo

--- Aposta/WebContent/result.jsp
+++ Aposta/WebContent/result.jsp
@@ -12,33 +12,34 @@
 		<h2> Aberto As Apostas Para A Copa Das Confederações</h2>
 		<h3> Favor Confira os Seus Dados e Suas Apostas</h3></br>
 		<p class="Intro">
-		Nome: ${param.nome} </br>
+		Nome: ${nome} </br>
 		Sexo: ${param.sexo} </br>
 		Email: ${param.email} </br>
 		</p>
 		<div id="grupoA">
 		<p>
 		<h3>Primeira Fase Grupo "A"</h3><br/>
-		Sábado 15/06 16:00 <img src="brasil.png" />Brasil ${param.Brasil1} X ${param.Japao1} Japão <img src="japao.png" /><br/>
-		Domingo 16/06 16:00 <img src="mexico.png" />Mexico ${param.Mexico1} X ${param.Italia1} Itália <img src="italia.png" />	<br/>
-		Quarta 19/06 16:00 <img src="brasil.png" />Brasil ${param.Brasil2} X ${param.Mexico2} México <img src="mexico.png" /><br/>
-		Quarta 19/06 19:00 <img src="italia.png" />Itália ${param.Italia2} X ${param.Japao2} Japão <img src="japao.png" /><br/>
-		Sabado 22/06 16:00 <img src="japao.png" />Japão ${param.Japao3} X ${param.Mexico3} México <img src="mexico.png" /><br/>	
-		Sabado 22/06 16:00 <img src="italia.png" />Itália ${param.Italia3} X ${param.Brasil3} Brasil <img src="brasil.png" />
+		Sábado 15/06 16:00 <img src="brasil.png" />Brasil ${Brasil1} X ${Japao1} Japão <img src="japao.png" /><br/>
+		Domingo 16/06 16:00 <img src="mexico.png" />Mexico ${Mexico1} X ${Italia1} Itália <img src="italia.png" />	<br/>
+		Quarta 19/06 16:00 <img src="brasil.png" />Brasil ${Brasil2} X ${Mexico2} México <img src="mexico.png" /><br/>
+		Quarta 19/06 19:00 <img src="italia.png" />Itália ${Italia2} X ${Japao2} Japão <img src="japao.png" /><br/>
+		Sabado 22/06 16:00 <img src="japao.png" />Japão ${Japao3} X ${Mexico3} México <img src="mexico.png" /><br/>	
+		Sabado 22/06 16:00 <img src="italia.png" />Itália ${Italia3} X ${Brasil3} Brasil <img src="brasil.png" />
 		 </p>
 		</div>
 		<div id="grupoB">
 		<p>
 		<h3>Primeira Fase Grupo "B"</h3><br/>
-		Domingo 16/06 19:00 <img src="espanha.png" />Espanha ${param.Espanha1} X ${param.Uruguai1} Uruguai <img src="uruguai.png" /><br/>
-		Segunda 17/06 16:00 <img src="taiti.png" />Taiti ${param.Taiti1} X ${param.Nigeria1} Nigéria <img src="nigeria.png" />	<br/>
-		Quinta 20/06 16:00 <img src="espanha.png" />Espanha ${param.Espanha2} X ${param.Taiti2} Taiti <img src="taiti.png" /><br/>
-		Quinta 20/06 19:00 <img src="nigeria.png" />Nigéria ${param.Nigeria2} X ${param.Uruguai2} Uruguai <img src="uruguai.png" /><br/>
-		Domingo 23/06 16:00 <img src="nigeria.png" />Nigéria ${param.Nigeria3} X ${param.Espanha3} Espanha <img src="espanha.png" /><br/>	
-		Domingo 23/06 16:00 <img src="uruguai.png" />Uruguai ${param.Uruguai3} X ${param.Taiti3} Taiti <img src="taiti.png" /><br/>
+		Domingo 16/06 19:00 <img src="espanha.png" />Espanha ${Espanha1} X ${Uruguai1} Uruguai <img src="uruguai.png" /><br/>
+		Segunda 17/06 16:00 <img src="taiti.png" />Taiti ${Taiti1} X ${Nigeria1} Nigéria <img src="nigeria.png" />	<br/>
+		Quinta 20/06 16:00 <img src="espanha.png" />Espanha ${Espanha2} X ${Taiti2} Taiti <img src="taiti.png" /><br/>
+		Quinta 20/06 19:00 <img src="nigeria.png" />Nigéria ${Nigeria2} X ${Uruguai2} Uruguai <img src="uruguai.png" /><br/>
+		Domingo 23/06 16:00 <img src="nigeria.png" />Nigéria ${Nigeria3} X ${Espanha3} Espanha <img src="espanha.png" /><br/>	
+		Domingo 23/06 16:00 <img src="uruguai.png" />Uruguai ${Uruguai3} X ${Taiti3} Taiti <img src="taiti.png" /><br/>
 		</p>
 		</div>
 		<p class="submit"> <input type="submit" value="Confirmar" /> <br/></p>
+		</form>
 		<footer>© 2013-2013 Todos Os direitos Reservados à André Ravagnani. Contato andreskiter@gmail.com</footer>
 	
 	</body>
--- Aposta/src/controle/Oi.java
+++ Aposta/src/controle/Oi.java
@@ -1,4 +1,4 @@
-package br.com.agenda;
+package controle;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -21,41 +21,94 @@ public class Oi extends HttpServlet {
 			out.println("<html><body><h1> A data do acesso é " + acesso + "</h1></body></html>" );	
 		}
 		public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
-			String idade = request.getParameter("idade");
-			String sexo = request.getParameter("sexo");
-			String email = request.getParameter("email");
 			String nome = request.getParameter("nome");
+			request.setAttribute("nome", nome);
+			
+			String idade = request.getParameter("idade");
+			request.setAttribute("idade", idade);
+			
+			String sexo = request.getParameter("sexo");
+			request.setAttribute("sexo", sexo);
+			
+			String email = request.getParameter("email");
+			request.setAttribute("email", email);
+			
 			String Brasil1 = request.getParameter("Brasil1");
+			request.setAttribute("Brasil1", Brasil1);
+
 			String Brasil2 = request.getParameter("Brasil2");
+			request.setAttribute("Brasil2", Brasil2);
+
 			String Brasil3 = request.getParameter("Brasil3");
+			request.setAttribute("Brasil3", Brasil3);
+
 			String Italia1 = request.getParameter("Italia1");
+			request.setAttribute("Italia1", Italia1);
+
 			String Italia2 = request.getParameter("Italia2");
+			request.setAttribute("Italia2", Italia2);
+
 			String Italia3 = request.getParameter("Italia3");
+			request.setAttribute("Italia3", Italia3);
+			
 			String Espanha1 = request.getParameter("Espanha1");
+			request.setAttribute("Espanha1", Espanha1 );
+
 			String Espanha2 = request.getParameter("Espanha2");
+			request.setAttribute("Espanha2", Espanha2);
+
 			String Espanha3 = request.getParameter("Espanha3");
+			request.setAttribute("Espanha3", Espanha3);
+
 			String Japao1 = request.getParameter("Japao1");
+			request.setAttribute("Japao1", Japao1);
+
 			String Japao2 = request.getParameter("Japao2");
+			request.setAttribute("Japao2", Japao2);
+
 			String Japao3 = request.getParameter("Japao3");
+			request.setAttribute("Japao3", Japao3);
+
 			String Mexico1 = request.getParameter("Mexico1");
+			request.setAttribute("Mexico1", Mexico1);
+
 			String Mexico2 = request.getParameter("Mexico2");
+			request.setAttribute("Mexico2", Mexico2);
+
 			String Mexico3 = request.getParameter("Mexico3");
+			request.setAttribute("Mexico3", Mexico3);
+
 			String Taiti1 = request.getParameter("Taiti1");
+			request.setAttribute("Taiti1", Taiti1);
+
 			String Taiti2 = request.getParameter("Taiti2");
+			request.setAttribute("Taiti2", Taiti2);
+
 			String Taiti3 = request.getParameter("Taiti3");
+			request.setAttribute("Taiti3", Taiti3);
+
 			String Uruguai1 = request.getParameter("Uruguai1");
+			request.setAttribute("Uruguai1", Uruguai1);
+
 			String Uruguai2 = request.getParameter("Uruguai2");
+			request.setAttribute("Uruguai2", Uruguai2);
+
 			String Uruguai3 = request.getParameter("Uruguai3");
+			request.setAttribute("Uruguai3", Uruguai3);
+
 			String Nigeria1 = request.getParameter("Nigeria1");
+			request.setAttribute("Nigeria1", Nigeria1);
+
 			String Nigeria2 = request.getParameter("Nigeria2");
+			request.setAttribute("Nigeria2", Nigeria2);
+
 			String Nigeria3 = request.getParameter("Nigeria3");
-			PrintWriter out = response.getWriter();
+			request.setAttribute("Nigeria3", Nigeria3);
+			
 			
 			 RequestDispatcher dispatcher = request.getRequestDispatcher("result.jsp"); 
 			 dispatcher.forward(request,response);
-			
-			out.println("<html><head><title> Casa De Apostas Ravagnani</title></head><body><h1>Bem Vindo A Casa De Apostas Ravagnani</h1><h2> Aberto As Apostas Para A Copa Das Confederações</h2><p> Favor Confirme os Seus Dados</br>Nome: "+ nome + " </br>Sexo: " + sexo + "</br>Email: " + email + "</body></html>");
-		}
+			}
 	}
 
 
